e37553dedd2342ced467835e8e8685ae6a9daedf
Michael Colenso <michael.colenso@lbox.com>
2013-02-19 22:14:11 -0800

    initial commit

--- client/www/app/util/HTMLUtil.js
+++
@@ -1,8 +0,0 @@
-window.HTMLUtil = {
-    encode: function (value){
-        return $('<div/>').text(value).html();
-    },
-    decode: function(value){
-        return $('<div/>').html(value).text();
-    }
-}
